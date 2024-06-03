ce716a9f455138de3cc24fdc4e390ef7ce2ef7fe
Rosie Burnett <rosie.victoria.burnett@gmail.com>
2024-06-03 08:36:06 +0200

    Update myCode.js

--- myCode.js
+++ myCode.js
@@ -1,1 +1,47 @@
-clear();
+
+//function syntax declared and called
+rosie = function () {
+    return "Good Morning!";
+
+}
+
+console.log(rosie());
+
+//if else ...bolean print
+const sunShine = true;
+
+if (sunShine) {
+    console.log ('use sunscrean');
+}
+else {
+    console.log ('wear a jumper');
+};
+
+
+const temperatureDegrees = 30;
+
+if (temperatureDegrees > 20){
+    console.log ('No jacket needed');
+}
+else {
+    console.log ('Wear your jacket')
+}
+
+
+
+//loop numers 5-10 counter
+for (let counter = 5; counter < 11; counter++)
+    {
+        console.log(counter);
+    }
+
+
+
+
+
+//create an array and call item by numer value
+const dreamVacations = ["Morocco","Bali","Norway"];
+
+console.log (dreamVacations[1]);
+
+
